c1ed1258e87f58405e7dea89f360a778036aaf28
ihmnoyon <noyonhasan320@gmail.com>
2024-07-07 23:35:54 +0600

    database added

--- Mamar_bank/settings.py
+++ Mamar_bank/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import environ
 env = environ.Env()
@@ -95,17 +95,25 @@ WSGI_APPLICATION = 'Mamar_bank.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
 
+
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://mamarbank_3tql_user:d4pRGr2HvtIvM8cbQ33076ykJv0sN2yC@dpg-cq5d2euehbks73bnanig-a.oregon-postgres.render.com/mamarbank_3tql',
+        
+    )
+}
 
 
 
